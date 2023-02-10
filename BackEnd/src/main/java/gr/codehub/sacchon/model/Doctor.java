d10317fc4ab32ac27c2c1606938109514bf9005e
Christoforos Gravas <gravas_ch@yahoo.gr>
2023-02-10 15:29:47 +0200

    Added classes and the attributes in Patient class

---
+++ BackEnd/src/main/java/gr/codehub/sacchon/model/Doctor.java
@@ -0,0 +1,4 @@
+package gr.codehub.sacchon.model;
+
+public class Doctor {
+}
---
+++ BackEnd/src/main/java/gr/codehub/sacchon/model/Patient.java
@@ -0,0 +1,15 @@
+package gr.codehub.sacchon.model;
+
+import java.util.Date;
+
+public class Patient {
+    private String firstname;
+    private String lastName;
+    private String address;
+    private String city;
+    private Date date;
+    private String diabetesTypes;
+    private boolean isActive;
+
+
+}
