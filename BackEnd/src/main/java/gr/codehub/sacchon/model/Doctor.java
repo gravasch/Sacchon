9d04e11c0cb479476c95d816dc76cf68c825e4b6
Malvina Zatka <malvzatk@csd.auth.gr>
2023-02-10 19:27:41 +0200

    first args to Doctor class

--- BackEnd/src/main/java/gr/codehub/sacchon/model/Doctor.java
+++ BackEnd/src/main/java/gr/codehub/sacchon/model/Doctor.java
@@ -1,4 +1,18 @@
 package gr.codehub.sacchon.model;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Doctor {
+    private String firstName="";
+    private String lastName="";
+    private String username="";
+    private Date dateOfBirth;
+    private Boolean isActive = false;
 }
--- BackEnd/src/test/java/gr/codehub/sacchon/SacchonApplicationTests.java
+++ BackEnd/src/test/java/gr/codehub/sacchon/SacchonApplicationTests.java
@@ -1,13 +1,13 @@
 package gr.codehub.sacchon;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SacchonApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SacchonApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
