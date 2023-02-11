1805831022793d23de4024c18aa3d640b15d611b
Nikos Takos <takosnikolaos@gmail.com>
2023-02-11 20:24:21 +0200

    Repositories package and content creation.

--- BackEnd/src/main/java/gr/codehub/sacchon/model/Doctor.java
+++ BackEnd/src/main/java/gr/codehub/sacchon/model/Doctor.java
@@ -8,6 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
+
 import java.util.Date;
 @Data
 @AllArgsConstructor
--- BackEnd/src/main/java/gr/codehub/sacchon/repository/GlucoseRepository.java
+++ BackEnd/src/main/java/gr/codehub/sacchon/repository/GlucoseRepository.java
@@ -2,5 +2,6 @@ package main.java.gr.codehub.sacchon.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface GlucoseRepository extends JpaRepository<gr.codehub.sacchon.model.Glucose, Long> {
 }
