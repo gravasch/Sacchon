ef24ceb167b411d756daca31ff2c3732532bc715
Nikos Takos <takosnikolaos@gmail.com>
2023-02-11 19:53:50 +0200

    Classes update with values , annotations and ID

--- BackEnd/src/main/java/gr/codehub/sacchon/model/Carb.java
+++ BackEnd/src/main/java/gr/codehub/sacchon/model/Carb.java
@@ -1,4 +1,24 @@
 package gr.codehub.sacchon.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Carb {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long carbId;
+    private double gram;
+    private Date date;
+
 }
--- BackEnd/src/main/java/gr/codehub/sacchon/model/Patient.java
+++ BackEnd/src/main/java/gr/codehub/sacchon/model/Patient.java
@@ -1,8 +1,23 @@
 package gr.codehub.sacchon.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
 import java.util.Date;
 
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Patient {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long patientId;
     private String firstname;
     private String lastName;
     private String address;
