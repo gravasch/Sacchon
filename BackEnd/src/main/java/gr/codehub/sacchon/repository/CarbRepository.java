12f136c2d41e20e95313adfa0154798a0b5fffe2
Nikos Takos <takosnikolaos@gmail.com>
2023-02-11 20:14:41 +0200

    Classes update with values , annotations and ID

---
+++ BackEnd/src/main/java/gr/codehub/sacchon/repository/CarbRepository.java
@@ -0,0 +1,6 @@
+package main.java.gr.codehub.sacchon.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarbRepository extends JpaRepository<gr.codehub.sacchon.model.Carb, Long> {
+}
---
+++ BackEnd/src/main/java/gr/codehub/sacchon/repository/PatientRepository.java
@@ -0,0 +1,6 @@
+package main.java.gr.codehub.sacchon.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientRepository extends JpaRepository<gr.codehub.sacchon.model.Patient, Long> {
+}
