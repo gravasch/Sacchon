a5860f585d21c5871591dc3ad3c8a0335163e36d
evangeliamilossi <evangelia.milossi@gmail.com>
2023-03-01 20:55:31 +0200

    Added Carbs configuration

--- BackEnd/src/main/java/gr/codehub/sacchon/Configuration/InitialConfiguration.java
+++ BackEnd/src/main/java/gr/codehub/sacchon/Configuration/InitialConfiguration.java
@@ -2,6 +2,7 @@ package gr.codehub.sacchon.Configuration;
 
 
 import gr.codehub.sacchon.model.*;
+import gr.codehub.sacchon.repository.CarbRepository;
 import gr.codehub.sacchon.repository.ConsultationRepository;
 import gr.codehub.sacchon.repository.DoctorRepository;
 import gr.codehub.sacchon.repository.PatientRepository;
@@ -18,7 +19,7 @@ public class InitialConfiguration {
 
         @Bean
         CommandLineRunner commandLineRunner(
-                DoctorRepository doctorRepository, ConsultationRepository consultationRepository, PatientRepository patientRepository) {
+                DoctorRepository doctorRepository, ConsultationRepository consultationRepository, PatientRepository patientRepository,CarbRepository carbRepository) {
             return args -> {
                 Doctor doctor1 = new Doctor(  1L,  "Malvina", "Zatka", "mzatka", LocalDate.now(),
                         DoctorType.DOCTOR, true ,null, null);
@@ -42,6 +43,14 @@ public class InitialConfiguration {
                 Consultation consultation2 = new Consultation( 2L,"zoloft",  new BigDecimal("2.05"), "three every day",LocalDate.now() ,doctor2, null);
 
                 consultationRepository.saveAll(List.of(consultation1, consultation2));
+
+                CarbMeasurements carbMeasurements1 = new CarbMeasurements(1L,LocalDate.now(), new BigDecimal("133.3"),patient1);
+                CarbMeasurements carbMeasurements2 = new CarbMeasurements(2L,LocalDate.of(2023,02,28), new BigDecimal("142.9"),patient1);
+                CarbMeasurements carbMeasurements3 = new CarbMeasurements(3L,LocalDate.now(), new BigDecimal("125.6"),patient2);
+                CarbMeasurements carbMeasurements4 = new CarbMeasurements(4L,LocalDate.of(2023,02,28), new BigDecimal("130.4"),patient2);
+
+
+                List<CarbMeasurements> list3 = carbRepository.saveAll(List.of(carbMeasurements1,carbMeasurements2,carbMeasurements3,carbMeasurements4));
 
 
                 System.out.println(consultation1);
